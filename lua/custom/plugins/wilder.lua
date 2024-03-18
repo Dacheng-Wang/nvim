b418e40063ec014c129af02398548c86f403b490
Dacheng Wang <dacheng.wang@outlook.com>
2024-03-17 19:14:18 -0500

    changed wilder.nvim settings

--- lua/custom/plugins/wilder.lua
+++ lua/custom/plugins/wilder.lua
@@ -7,25 +7,24 @@ return {
     wilder.set_option(
       'renderer',
       wilder.renderer_mux {
-        [':'] = wilder.popupmenu_renderer(wilder.popupmenu_palette_theme {
-          -- 'single', 'double', 'rounded' or 'solid'
-          -- can also be a list of 8 characters, see :h wilder#popupmenu_palette_theme() for more details
-          border = 'rounded',
-          max_height = '75%', -- max height of the palette
-          min_height = 0, -- set to the same as 'max_height' for a fixed height window
-          prompt_position = 'top', -- 'top' or 'bottom' to set the location of the prompt
-          reverse = 0, -- set to 1 to reverse the order of the list, use in combination with 'prompt_position'
-          highlighter = {
-            wilder.lua_fzy_highlighter(), -- requires fzy-lua-native vim plugin found
-          },
+        [':'] = wilder.popupmenu_renderer(wilder.popupmenu_border_theme {
+          highlighter = wilder.lua_fzy_highlighter(),
           highlights = {
             accent = wilder.make_hl('WilderAccent', 'Pmenu', { { a = 1 }, { a = 1 }, { foreground = '#f4468f' } }),
+            border = 'Normal', -- highlight to use for the border
           },
+          left = {
+            ' ',
+            wilder.popupmenu_devicons(),
+          },
+          right = {
+            ' ',
+            wilder.popupmenu_scrollbar(),
+          },
+          border = 'rounded',
         }),
         ['/'] = wilder.wildmenu_renderer {
-          highlighter = {
-            wilder.lua_fzy_highlighter(), -- or wilder.lua_fzy_highlighter(),
-          },
+          highlighter = wilder.lua_fzy_highlighter(),
           highlights = {
             accent = wilder.make_hl('WilderAccent', 'Pmenu', { { a = 1 }, { a = 1 }, { foreground = '#f4468f' } }),
           },
